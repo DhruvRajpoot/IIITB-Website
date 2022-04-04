c2baaca5226214b6a847852fef4c27f867d46466
Dhruv <dhruv.kutarya@gmail.com>
2022-04-04 15:43:30 +0530

    Visitors Random number correct

--- IIITBWebsiteJS.js
+++ IIITBWebsiteJS.js
@@ -1,10 +1,9 @@
 
         // VISITORS Random number generator JS //
-
-            function random() {
-                let m = Math.random();
-                let a = 177800;
-                let b = 179900;
-                let r = a + (b - a) * m;
-                document.getElementById("rand").innerHTML = Math.floor(r);
-            }
+            document.addEventListener('DOMContentLoaded',(event)=>{
+                    let m = Math.random();
+                    let a = 177800;
+                    let b = 179900;
+                    let r = a + (b - a) * m;
+                    document.getElementById("rand").innerHTML = Math.floor(r);   
+            });
